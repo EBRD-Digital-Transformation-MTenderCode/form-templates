5ce0a0535cd0f30b8a1e481a6cc6cfcb138e677b
Volkov Dima <mr.rotberry@gmail.com>
2018-07-24 14:58:25 +0300

    add documents uri to cn

--- src/main/kotlin/com/procurement/formsservice/model/cn/Data.kt
+++ src/main/kotlin/com/procurement/formsservice/model/cn/Data.kt
@@ -19,6 +19,7 @@ data class Uris(
     val locality: String, // /locality?lang=langFromRequest&region=
     val registrationScheme: String, // /registration-scheme?lang=langFromRequest&country=
     val currency: String, // /currency?lang=langFromRequest&country=(Buyer.Address.Country.id)
+    val document: String, // /doc-types?lang=langFromRequest&entity=tender
     val unitClass: String, // /unit-class?lang=langFromRequest
     val unit: String, // /unit?lang=langFromRequest&unitClass=
     val cpv: String, // cpv?lang=langFromRequest&code=(EI.tender.classification.id)
