47dad2ab98b0653ae26255089c39636ba4d98db5
Volkov Dima <mr.rotberry@gmail.com>
2018-07-24 17:45:11 +0300

    fix documents for cn and bid

--- src/main/kotlin/com/procurement/formsservice/model/bid/Data.kt
+++ src/main/kotlin/com/procurement/formsservice/model/bid/Data.kt
@@ -16,8 +16,7 @@ data class Uris(
     val region: String, // /region?lang=langFromRequest&country=
     val locality: String, // /locality?lang=langFromRequest&region=
     val registrationScheme: String, // /registration-scheme?lang=langFromRequest&country=
-    val currency: String, // /currency?lang=langFromRequest&country=(Buyer.Address.Country.id)
-    val document: String // /doc-types?lang=langFromRequest&entity=bid
+    val currency: String // /currency?lang=langFromRequest&country=(Buyer.Address.Country.id)
 )
 
 data class Amount(
