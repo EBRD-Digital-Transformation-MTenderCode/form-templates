7f769105699be6050ed6bacea80e4f064a797354
Maxim Sambulat <maxim.sambulat@gmail.com>
2018-07-25 13:03:13 +0300

    Refactoring the Pebble extension.

--- src/main/kotlin/com/procurement/formsservice/pebble/extension/FormExtension.kt
+++ src/main/kotlin/com/procurement/formsservice/pebble/extension/FormExtension.kt
@@ -2,24 +2,12 @@ package com.procurement.formsservice.pebble.extension
 
 import com.mitchellbosecke.pebble.extension.AbstractExtension
 import com.mitchellbosecke.pebble.extension.Function
+import com.procurement.formsservice.pebble.function.AttributeIf
+import com.procurement.formsservice.pebble.function.FindSubString
 
 object FormExtension : AbstractExtension() {
     override fun getFunctions(): Map<String, Function> = mutableMapOf<String, Function>().apply {
         this["findSubString"] = FindSubString
-    }
-}
-
-object FindSubString : Function {
-    private const val TEXT_PARAMETER = "text"
-    private const val REGEX_PARAMETER = "regex"
-    override fun getArgumentNames(): List<String> = mutableListOf<String>().apply {
-        add(TEXT_PARAMETER)
-        add(REGEX_PARAMETER)
-    }
-
-    override fun execute(parameters: MutableMap<String, Any>): Any {
-        val text = parameters[TEXT_PARAMETER] as String
-        val regex = (parameters[REGEX_PARAMETER] as String).toRegex()
-        return regex.matches(text)
+        this["attributeIf"] = AttributeIf
     }
 }
