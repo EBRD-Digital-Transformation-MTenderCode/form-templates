2c55c0a7d40b1fe5a29e7a219de7343352eae118
Volkov Dima <mr.rotberry@gmail.com>
2018-07-26 14:36:31 +0300

    fix enquiry

--- src/main/kotlin/com/procurement/formsservice/model/enquiry/Data.kt
+++ src/main/kotlin/com/procurement/formsservice/model/enquiry/Data.kt
@@ -13,7 +13,5 @@ data class Uris(
     val country: String, // /country?lang=langFromRequest
     val region: String, // /region?lang=langFromRequest&country=
     val locality: String, // /locality?lang=langFromRequest&region=
-    val registrationScheme: String, // /registration-scheme?lang=langFromRequest&country=
-    val currency: String, // /currency?lang=langFromRequest&country=(Buyer.Address.Country.id)
-    val document: String // /doc-types?lang=langFromRequest&entity=bid
+    val registrationScheme: String // /registration-scheme?lang=langFromRequest&country=
 )
