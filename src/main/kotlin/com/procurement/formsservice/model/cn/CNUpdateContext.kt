3ed790210f64ddfdc31913d366a0508147c87f00
volkov <mr.rotberry@gmail.com>
2018-09-12 15:49:48 +0300

    bugfix

--- src/main/kotlin/com/procurement/formsservice/model/cn/CNUpdateContext.kt
+++ src/main/kotlin/com/procurement/formsservice/model/cn/CNUpdateContext.kt
@@ -187,7 +187,7 @@ class CNUpdateContext(
                 val type: String, // CN.tender.documents[relatedLots[0] = lot.id].documentType
                 val title: String?, // CN.tender.documents[relatedLots[0] = lot.id].title
                 val description: String?, // CN.tender.documents[relatedLots[0] = lot.id].description
-                val relatedLots: List<String> // CN.tender.documents[relatedLots[0] = lot.id].relatedLots
+                val relatedLots: String // CN.tender.documents[relatedLots[0] = lot.id].relatedLots[0]
             )
         }
 
