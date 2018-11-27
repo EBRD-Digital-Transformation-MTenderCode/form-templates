c9de15b62e036a5e244a64ca6c85e97b24e53ee9
volkov <mr.rotberry@gmail.com>
2018-11-27 20:42:08 +0200

    update ac schema complete and add data WIP

--- src/main/kotlin/com/procurement/formsservice/model/ac/ACUpdateContext.kt
+++ src/main/kotlin/com/procurement/formsservice/model/ac/ACUpdateContext.kt
@@ -2,15 +2,43 @@ package com.procurement.formsservice.model.ac
 
 class ACUpdateContext (
     val parameters: Parameters,
+    val lotId: String, // AC.awards[0].relatedLots[0]
+    val contract: Contract,
     val buyer: Buyer,
     val supplier: Supplier,
     val itemUris: ItemUris,
     val currency: String // AC.planning.budget.budgetSource[0].currency
 ) {
     data class Parameters (
-        val ocid: String, // ocds-t1s2t3-MD-1532010121824-AC-1532010122650
-        val lotId: String // 1a8e2f80-8b5f-11e8-a48b-9f2980d5bbdd
+        val ocid: String // ocds-t1s2t3-MD-1532010121824-AC-1532010122650
     )
+
+    data class Contract(
+        val title: String?, // AC.contracts[0].title
+        val description: String?, // AC.contracts[0].description
+        val period: Period,
+        val documents: List<Document>? // AC.contracts[0].documents
+    ) {
+        data class Period(
+            val startDate: String?, // AC.contracts[0].period.startDate
+            val endDate: String? // AC.contracts[0].period.endDate
+        )
+
+        data class Document(
+            val id: String, // AC.contracts[0].documents[*].id
+            val type: String, // AC.contracts[0].documents[*].documentType
+            val title: String?, // AC.contracts[0].documents[*].title
+            val description: String?, // AC.contracts[0].documents[*].description
+            val relatedLot: String, // AC.contracts[0].documents[*].relatedLots[0]
+            val confirmationRequest: ConfirmationRequest? // AC.contracts[0].confirmationRequests
+        ) {
+            data class ConfirmationRequest(
+                val id: String, // AC.contracts[0].confirmationRequests[relatedItem==document.id].id
+                val relatedItem: String, // AC.contracts[0].confirmationRequests[relatedItem==document.id].relatedItem
+                val source: String // AC.contracts[0].confirmationRequests[relatedItem==document.id].source
+            )
+        }
+    }
 
     data class Buyer (
         val uris: Uris,
