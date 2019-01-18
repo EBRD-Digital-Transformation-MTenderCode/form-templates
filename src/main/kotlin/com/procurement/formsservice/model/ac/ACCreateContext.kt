7ee7cd2b52b14898d450717479616f4d90648b36
annisokay <annisokay97@gmail.com>
2019-01-18 18:28:20 +0200

    bugfix

--- src/main/kotlin/com/procurement/formsservice/model/ac/ACCreateContext.kt
+++ src/main/kotlin/com/procurement/formsservice/model/ac/ACCreateContext.kt
@@ -9,7 +9,7 @@ class ACCreateContext(
         val ocid: String // ocds-t1s2t3-MD-1532010121824-EV-1532010122650
     )
     data class Contract(
-        val id: String //EV.contracts[*].id
+        val id: String //EV.contracts[statusDetails == "contractProject"].id
     )
 }
 
